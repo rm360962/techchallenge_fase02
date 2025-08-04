8396daa4b890eae4f9d78cb94d1b37d0f87c5cf2
rm360962 <rm360962@fiap.com.br>
2025-08-03 21:34:00 -0300

    Feat: alteração para teste da action

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('', (req, res) => {
     res.status(200).send({
-        mensagem: "Bem vindo a api"
+        mensagem: "Bem vindo a ap"
     });
 });
 
