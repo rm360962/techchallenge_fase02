7aa8de4b605f74cae4a9d90e995cd7bc5d9ca1d3
rm360962 <rm360962@fiap.com.br>
2025-08-05 19:44:59 -0300

    teste

--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -4,6 +4,12 @@ const router = express.Router();
 
 router.get('', (req, res) => {
     res.status(200).send({
+        mensagem: "Bem vindo a ap"
+    });
+});
+
+router.get('/teste', (req, res) => {
+    res.status(200).send({
         mensagem: "Bem vindo a api"
     });
 });
